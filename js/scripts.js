e0f9da7e6ace4eb75679c6cbcbd3c80d3346a8cf
mila1601 <franciskimari@gmail.com>
2019-06-17 09:19:21 +0300

    added js/

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+function Pizza(pizzaSize,pizzaToppings) {
+  this.pizzaSize = pizzaSize;
+  this.pizzaToppings = pizzaToppings;
+}
+
+Pizza.prototype.sizeCost = function () {
+  var pizzaSizePrice = 1000;
+  if(this.pizzaSize === "Large" ) {
+     return pizzaSizePrice =1000 ;
+  } 
+   
+  else {
+    return pizzaSizePrice =800 ;
+  }
+
+}
+// pizzaToppings
+Pizza.prototype.toppingsCost = function () {
+var pizzaPrice =200 ;
+pizzaPrice += this.pizzaToppings.length ;
+return pizzaPrice;
+}
+
+Pizza.prototype.totalPizzaCost = function (testPizza) {
+  return "The cost of your pizza will be" + " " + (this.sizeCost() + this.toppingsCost()) + " sh."+"Thank U And Welcome Again.";
+}
+
+
+// my jquerry
+
+$(document).ready(function() {
+  $('form#new-pizza').submit(function(event) {
+    event.preventDefault();
+
+    var inputtedSize = $('#pizzaSize input:radio:checked').val();
+    var checkedToppings = $('input[name=toppings]:checked');
+    var inputtedToppings = [];
+  $(checkedToppings).each(function() {
+    inputtedToppings.push($(this).val());
+    });
+    var newPizza = new Pizza(inputtedSize, inputtedToppings);
+
+    $("#order").text(newPizza.totalPizzaCost());
+
+  });
+
+});
